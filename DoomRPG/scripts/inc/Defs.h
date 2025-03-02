73872d2674cfe18b66be11f7ef7860b881320544
RonDallas <32035822+RonDallas@users.noreply.github.com>
2025-03-02 20:44:34 +0200

    Pandemonia monsters 2.4.1 to last rebalance_dev

--- DoomRPG/scripts/inc/Defs.h
+++ DoomRPG/scripts/inc/Defs.h
@@ -443,7 +443,7 @@ typedef enum
 #define MAX_DEF_MONSTERS_CH     135
 #define MAX_DEF_MONSTERS_RM     38
 #define MAX_TEMP_MONSTERS       150
-#define MAX_DEF_MONSTERS_PANDM  113
+#define MAX_DEF_MONSTERS_PANDM  132
 #define MAX_MEGABOSSES_DF       2
 #define MAX_MEGABOSSES_CH       2
 #define MAX_SKILLLEVELS_DF		6
